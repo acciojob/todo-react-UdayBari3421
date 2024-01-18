d660ed0b493baf624e08495a95e9d1970bba968f
UdayBari3421 <udaybari2003@gmail.com>
2024-01-18 16:52:13 +0530

    submitting code-Thu Jan 18 2024 16:52:11 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
-export default App
+  const handleAdd = (e) => {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  };
+
+  const handleDelete = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index));
+  };
+
+  return (
+    <div className="box">
+      <form className="subox">
+        <p>To-Do-List</p>
+        <input
+          type="text"
+          id="todo"
+          name="todo"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button onClick={handleAdd}>Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li key={index}>
+              <p>{todo}</p>
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
